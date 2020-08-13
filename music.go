534a51434cd431bb1f587abf9ab30ae8ad3fe0b8
polidog <polidogs@gmail.com>
2020-08-13 23:44:46 +0900

    Change directory structure.

--- music.go
+++ music.go
@@ -1,92 +1,43 @@
-package music
+package main
 
 import (
-	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
 
-	"github.com/polidog/go-music/script"
+	"github.com/polidog/go-music/player"
 )
 
-// Track is 再生しているトラック情報
-type Track struct {
-	Album    string `json:"album"`
-	Artist   string `json:"artist"`
-	Name     string `json:"name"`
-	Category string `json:"category"`
-	Time     string `json:"time"`
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	os.Exit(run(args))
 }
 
-// Result 取得データ
-type Result struct {
-	Status      bool   `json:"status"`
-	PlayerState string `json:"player_state"`
-	Track       Track  `json:"track"`
-}
+func run(args []string) int {
+	if len(args) < 2 {
+		fmt.Println("argument error")
+		return 1
+	}
 
-// Player is music player
-type Player struct {
-	script script.Script
-}
-
-// NewPlayer is create a Player struct.
-func NewPlayer(service string) (Player, error) {
-	script, err := script.NewScript(service)
+	player, err := player.NewPlayer(args[0])
 	if err != nil {
-		return Player{}, err
+		fmt.Println(err)
+		return 1
 	}
 
-	return Player{
-		script: script,
-	}, nil
-}
+	res, errExec := player.Exec(args[1])
 
-func (p *Player) Track() (Result, error) {
-	return p.exec("track")
-}
-
-func (p *Player) Play() (Result, error) {
-	return p.exec("play")
-}
-
-func (p *Player) Pause() (Result, error) {
-	return p.exec("pause")
-}
-
-func (p *Player) Next() (Result, error) {
-	return p.exec("next")
-}
-
-func (p *Player) Previous() (Result, error) {
-	return p.exec("previous")
-}
-
-func (p *Player) State() (Result, error) {
-	return p.exec("state")
-}
-
-func (p *Player) IsPlaying() (Result, error) {
-	return p.exec("isPlaying")
-}
-
-func (p *Player) Exec(command string) (Result, error) {
-	return p.exec(command)
-}
-
-func (p *Player) exec(command string) (Result, error) {
-	str, err := p.script.Exec(command)
-	result := Result{
-		Track: Track{},
+	if errExec != nil {
+		fmt.Println(err)
+		return 1
 	}
 
-	if err != nil {
-		return result, err
-	}
+	fmt.Println("player_state:", res.PlayerState)
 
-	jsonErr := json.Unmarshal(str, &result)
-	if jsonErr != nil {
-		fmt.Println(command)
-		fmt.Println(str)
-		return result, jsonErr
-	}
-	return result, nil
+	fmt.Println("----------track-----------")
+	fmt.Println("Name:", res.Track.Name)
+	fmt.Println("Album:", res.Track.Album)
+	fmt.Println("Artist:", res.Track.Artist)
+	return 0
 }
---
+++ player/player.go
@@ -0,0 +1,92 @@
+package player
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/polidog/go-music/script"
+)
+
+// Track is 再生しているトラック情報
+type Track struct {
+	Album    string `json:"album"`
+	Artist   string `json:"artist"`
+	Name     string `json:"name"`
+	Category string `json:"category"`
+	Time     string `json:"time"`
+}
+
+// Result 取得データ
+type Result struct {
+	Status      bool   `json:"status"`
+	PlayerState string `json:"player_state"`
+	Track       Track  `json:"track"`
+}
+
+// Player is music player
+type Player struct {
+	script script.Script
+}
+
+// NewPlayer is create a Player struct.
+func NewPlayer(service string) (Player, error) {
+	script, err := script.NewScript(service)
+	if err != nil {
+		return Player{}, err
+	}
+
+	return Player{
+		script: script,
+	}, nil
+}
+
+func (p *Player) Track() (Result, error) {
+	return p.exec("track")
+}
+
+func (p *Player) Play() (Result, error) {
+	return p.exec("play")
+}
+
+func (p *Player) Pause() (Result, error) {
+	return p.exec("pause")
+}
+
+func (p *Player) Next() (Result, error) {
+	return p.exec("next")
+}
+
+func (p *Player) Previous() (Result, error) {
+	return p.exec("previous")
+}
+
+func (p *Player) State() (Result, error) {
+	return p.exec("state")
+}
+
+func (p *Player) IsPlaying() (Result, error) {
+	return p.exec("isPlaying")
+}
+
+func (p *Player) Exec(command string) (Result, error) {
+	return p.exec(command)
+}
+
+func (p *Player) exec(command string) (Result, error) {
+	str, err := p.script.Exec(command)
+	result := Result{
+		Track: Track{},
+	}
+
+	if err != nil {
+		return result, err
+	}
+
+	jsonErr := json.Unmarshal(str, &result)
+	if jsonErr != nil {
+		fmt.Println(command)
+		fmt.Println(str)
+		return result, jsonErr
+	}
+	return result, nil
+}
